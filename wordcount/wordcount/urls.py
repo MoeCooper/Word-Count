0d7994cfdc88e261f1dde590890dbec66b05f00c
Maurice Cooper <jc70240@gmail.com>
2020-05-10 03:27:21 -0500

    added css styling

--- wordcount/wordcount/urls.py
+++ wordcount/wordcount/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.homepage),
     path('count/', views.count, name='count')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
